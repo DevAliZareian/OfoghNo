729a82412391f63787db7ab79efe05fc9646dfaa
DevAliZareian <154817911+DevAliZareian@users.noreply.github.com>
2025-03-16 00:03:17 +0330

    feat: add 404 Not Found page
    - Created a fallback route for undefined paths
    - Designed a simple 404 page with navigation back to home
    - Integrated it with React Router

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center justify-center gap-4">
+      <img src="../../public/assets/404NotFound.svg" alt="" className="w-[90%]" />
+      <h1 className="text-2xl">PageNotFound!</h1>
+    </div>
+  );
+}
