2dbfae44561a5c90354bd38037d8412b1b422f37
DevAliZareian <154817911+DevAliZareian@users.noreply.github.com>
2025-03-16 02:00:55 +0330

    feat: setup a fallback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { routes } from "./routes";
+import FallBack from "./components/FallBack";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>fallback</div>}>
+      <Suspense fallback={<FallBack />}>
         <RouterProvider router={createBrowserRouter(routes)} />
       </Suspense>
     </QueryClientProvider>
---
+++ client/src/components/FallBack.tsx
@@ -0,0 +1,7 @@
+export default function FallBack() {
+  return (
+    <div className="relative w-full min-h-screen flex flex-col items-center justify-center bg-[#121212] text-white">
+      <p>FallBack</p>
+    </div>
+  );
+}
